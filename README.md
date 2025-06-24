17cba4cf9fa3ea25794724f8e27f4ac2ea4c3290
Bhargav <bhargav14feb@gmail.com>
2025-06-24 12:50:54 +0530

    python first chapter

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python
+<br>
+hello guys how are you this is everything i learnt in python for free!!
+
 everything I learnt about python!
---
+++ hello_world.py
@@ -0,0 +1,2 @@
+# hey everyone this is the beg of your python journey with me so i just want to say you all hello in tech way!
+print("hello world")
