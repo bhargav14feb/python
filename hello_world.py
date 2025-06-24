d7b5fd15441ce41dd9699d53045db6615f83ff06
Bhargav <bhargav14feb@gmail.com>
2025-06-24 13:00:56 +0530

    module, pip, good work

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,5 @@
-# hey everyone this is the beg of your python journey with me so i just want to say you all hello in tech way!
+# copy and paste your first code
 print("hello world")
+# module- it is a code written by someone else and we can just import it using pip
+# pip can be used for importing a code written by someone else, example "pip install flask", this can only be used in terminal.
+print("good work")
