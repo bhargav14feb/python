eeeae5828fba354877043d26ee6f256abf306542
Bhargav <bhargav14feb@gmail.com>
2025-06-24 13:45:19 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+# variable- more like a box to which contains datatypes 
+# datatypes- different data types
+a= 38 #integers
+b= 93.44 #flooting pointer number
+c= "bhargav" #string
+# d= true or false #boolean
+
+# there are many different types of data types (dictionaries, set, tuplets)
+
+# type function (used for identifying the data type)
+a="string"
+type(a)
