cb0fef920e00e9f0711e757390067201fe309f50
Bhargav <bhargav14feb@gmail.com>
2025-06-24 20:37:17 +0530

    conditional statement

---
+++ conditional_statement.py
@@ -0,0 +1,11 @@
+# conditional statements are one of the most useful thing in python
+# Conditional statement is consist of If, elif and else statement
+
+a= int(input("Enter your age:"))
+
+if a>=18 :
+    print("you are adult")
+elif a<0:
+    print("enter a valid age")
+else:
+    print("you are a kid")
