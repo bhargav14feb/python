9488b6dbb5627d9717b806acfed407de75d3e302
Bhargav <bhargav14feb@gmail.com>
2025-06-24 19:37:44 +0530

    python

--- hello_world.py
+++
@@ -1,5 +0,0 @@
-# copy and paste your first code
-print("hello world")
-# module- it is a code written by someone else and we can just import it using pip
-# pip can be used for importing a code written by someone else, example "pip install flask", this can only be used in terminal.
-print("good work")
---
+++ list_tuple.py
@@ -0,0 +1,2 @@
+# list is a datatype, it is used to store different types of data sets all togther
+list= ["hello world", 84, 48.44, "neuralnetwork"]
