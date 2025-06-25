a7d59b4b2f2bea787f3a2437ad79fb6efc65b3b5
Bhargav <bhargav14feb@gmail.com>
2025-06-25 16:41:18 +0530

    loops and its functions

--- loops.py
+++ loops.py
@@ -19,6 +19,7 @@ while (condition):
 # some projects under for loops 
 
 # table of n
+
 # n= int(input("Enter a number:"))
 
 # for i in range (1,11):
@@ -31,10 +32,46 @@ n=3
    *
   ***
  *****
-'''
-n= int(input("Enter a number:"))
+ '''
+# n= int(input("Enter a number:"))
 
-for i in range (1,n+1):
-    print(" "*(n-i), end="")
-    print("*"*(2*i-1), end="")
-    print("")
+# for i in range (1,n+1):
+#     print(" "*(n-i), end="")
+#     print("*"*(2*i-1), end="")
+#     print("")
+
+# virtual bank
+
+balance = int(input("Enter your bank balance: "))
+
+while True:
+    print("\nChoose")
+    print("1. Your bank balance")
+    print("2. Cash deposit")
+    print("3. Cash withdrawal")
+    print("4. Exit")
+
+    a = input("Enter your option: ")
+
+    if a == "1":
+        print(f"Your balance is ₹{balance}")
+
+    elif a == "2":
+        amount = float(input("Enter the amount you want to deposit: "))
+        balance += amount
+        print(f"Deposited ₹{amount}. New balance is ₹{balance}")
+
+    elif a == "3":
+        amount = float(input("Enter the amount you want to withdraw: "))
+        if amount > balance:
+            print("Insufficient balance!")
+        else:
+            balance -= amount
+            print(f"Withdrawn ₹{amount}. New balance is ₹{balance}")
+
+    elif a == "4":
+        print("Thanks for banking with us 🙏")
+        break
+
+    else:
+        print("Invalid option. Please enter 1 to 4.")
