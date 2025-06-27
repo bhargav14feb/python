106434ce5c2b856f5767a802a783999b51579294
Bhargav <bhargav14feb@gmail.com>
2025-06-27 20:06:31 +0530

    functus

---
+++ functions.py
@@ -0,0 +1,18 @@
+# there are two types of function in this tech world
+# 1. Is inbuilt function (len(), upper, lower, startswith )
+# 2. user defined function 
+'''
+def any variable():
+    your code 
+
+your variable 
+'''
+def functus():
+    a= int(input("Enter a number:"))
+    b= int(input("Enter a number:"))
+    c= int(input("Enter a number:"))
+                
+    average= (a+b+c)/3
+    print(average)
+
+functus()
